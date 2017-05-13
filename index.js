3a8902f1ce103d426b7f6a4f64b349940b3e429d
Adegoke Saidi A <rasheedsaidi@gmail.com>
2017-05-13 16:18:56 +0100

    Add test for auth-hook

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ app.set('port', process.env.PORT || 5000);
 app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
 app.set('view engine', 'hbs');
 //app.use(bodyParser.json({ verify: verifyRequestSignature }));
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use(session({
   cookieName: 'session',
