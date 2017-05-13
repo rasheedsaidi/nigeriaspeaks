62f822746b94f78d3db1f8ee5c95d668c6a92cd3
Adegoke Saidi A <rasheedsaidi@gmail.com>
2017-05-13 14:01:48 +0100

    Add test for auth-hook

--- index.js
+++ index.js
@@ -108,6 +108,7 @@ app.get('/auth-hook', function(req, res) {
 app.post('/auth-hook', function(req, res) {
     if(req.body && req.body.uid) {
       req.session.uid = req.body.uid;
+      res.send(req.body);
     }
     res.end();
 });
